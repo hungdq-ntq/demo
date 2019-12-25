79fad0d260d1c4b7843822a0bfa77232f840ec5a
vuongtaquoc <vuong.ta@ntq-solution.com.vn>
2019-12-25 10:26:38 +0700

    add api from json

--- routes/api/org.api.js
+++ routes/api/org.api.js
@@ -1,6 +1,11 @@
+const fs = require('fs');
+const path = require('path');
+
 const router = require('express').Router();
 
 const generator = require('../../service/org-chart-generator');
+
+const dataDir = path.resolve(__dirname, '../../data');
 
 router.get('/ping', (req, res) => res.status(200).send({ date: new Date(), message: 'PONG' }));
 
@@ -14,6 +19,38 @@ router.post('/user', (req, res) => {
 
 router.post('/refresh', (req, res) => {
     return res.status(200).send(generator.refreshData());
+});
+
+router.post('/departments/:id', (req, res, next) => {
+    const filePath = path.join(dataDir, `/orgs/${ req.params.id }`);
+
+    if (!fs.existsSync(filePath)) {
+        return res.sendStatus(404);
+    }
+
+    const departments = fs.readFileSync(filePath);
+
+    try {
+        res.json(JSON.parse(departments));
+    } catch (e) {
+        res.status(400).send(e);
+    }
+});
+
+router.post('/users/:id', (req, res, next) => {
+    const filePath = path.join(dataDir, `/users/${ req.params.id }`);
+
+    if (!fs.existsSync(filePath)) {
+        return res.sendStatus(404);
+    }
+
+    const users = fs.readFileSync(filePath);
+
+    try {
+        res.json(JSON.parse(users));
+    } catch (e) {
+        res.status(400).send(e);
+    }
 });
 
 module.exports = router;
